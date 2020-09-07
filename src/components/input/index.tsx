342449b428b47dd99c282cc8b4c5a34b2e6dd103
JuoJuo <348107781@qq.com>
2020-09-07 22:23:48 +0800

    bug fix

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,4 +1,4 @@
-import React, { HTMLAttributes } from 'react';
+import React, { HTMLAttributes, MutableRefObject, useMemo, useRef } from 'react';
 import 'bulma';
 import { MainColorTypes, sizeTypes } from "../button";
 import { getClassNames, getDefaultGlobalSize, getDefaultGlobalColor } from "../../utils";
@@ -28,6 +28,14 @@ export interface InputProps extends HTMLAttributes<HTMLInputElement> {
    * input类型,是否是password，默认是text
    */
   password?: boolean;
+  /**
+   * input框输入的值
+   */
+  value?: string;
+  /**
+   * input的change事件
+   */
+  onChange?: (string) => void;
 }
 
 export const Input: React.FC<InputProps> = (props) => {
@@ -37,28 +45,39 @@ export const Input: React.FC<InputProps> = (props) => {
     loading = false,
     disabled = false,
     password = false,
+    value = '',
+    onChange = (string) => {},
     ...leftProps
   } = props;
+
+  const inputRef: MutableRefObject<HTMLInputElement> = useRef<HTMLInputElement>();
+
+  const getInputJSX = () => {
+
+    function _onChange() {
+      onChange && onChange(inputRef.current.value);
+    }
+
+    return <input
+      className={`input ${getClassNames({ color: props.color, size: props.size, rounded: props.rounded })}`}
+      disabled={props.disabled}
+      type={ props.password ? 'password' : 'text'}
+      value={props.value}
+      onChange={_onChange}
+      ref={inputRef}
+      {...leftProps}
+    />
+  };
+
+  const inputJsx = useMemo(() => getInputJSX(), [color, size, rounded, disabled, password, value]);
 
   if (loading) {
     return (
       <div className={`control ${getClassNames({ loading, size })}`}>
-        <input
-          className={`input ${getClassNames({ color, size, rounded })}`}
-          disabled={disabled}
-          type={ password ? 'password' : 'text'}
-          {...leftProps}
-        />
+        {inputJsx}
       </div>
     );
   }
 
-  return (
-    <input
-      className={`input ${getClassNames({ color, size, rounded })}`}
-      disabled={disabled}
-      type={ password ? 'password' : 'text'}
-      {...leftProps}
-    />
-  );
+  return inputJsx;
 };
--- src/stories/input/input.stories.tsx
+++ src/stories/input/input.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import { Input, InputProps } from '../../components/input';
 import { Row } from '../../components/row';
@@ -9,15 +9,19 @@ export default {
   component: Input,
 } as Meta;
 
-const Template: Story<InputProps> = (args) => (
-  <>
-    <Row>
-      <Col span='6'>
-        <Input {...args} placeholder='please enter username'/>
-      </Col>
-    </Row>
-  </>
-);
+const Template: Story<InputProps> = (args) => {
+  const [name, setName] = useState('');
+
+  return (
+    <>
+      <Row>
+        <Col span='6'>
+          <Input {...args} placeholder='please enter username' value={name} onChange={setName}/>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export const InputDemo = Template.bind({});
 
