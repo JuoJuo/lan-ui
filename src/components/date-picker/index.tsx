99e3cef85ac00471ef4a77174c19e9dab475dbcc
JuoJuo <348107781@qq.com>
2020-09-07 22:27:25 +0800

    record for datepicker

--- src/components/date-picker/index.tsx
+++ src/components/date-picker/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef, useMemo } from 'react';
+import React, { useEffect, useState, useRef, useMemo, MutableRefObject } from 'react';
 import 'bulma';
 import { MainColorTypes, sizeTypes } from "../button";
 import { Input } from '../input';
@@ -18,10 +18,9 @@ export interface DatePickerProps {
 export function useClickOutside(ref: React.RefObject<HTMLElement>, handler: Function) {
   useEffect(() => {
     const listener = (event: MouseEvent) => {
-      if (!ref.current || ref.current.contains(event.target as Node)) {
-        return;
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        handler(event);
       }
-      handler(event);
     };
 
     window.addEventListener("click", listener);
@@ -30,64 +29,35 @@ export function useClickOutside(ref: React.RefObject<HTMLElement>, handler: Func
   }, [ref, handler]);
 }
 
-export function useStateAnimation(parentSetState: (v: boolean) => void, delay: number = 300): [boolean, (v: boolean) => void, () => void] {
-  const [state, setState] = useState(true);
-  const [innerClose, unmount] = useMemo(() => {
-    let timer: number;
-    let innerclose = (v: boolean) => {
-      setState(v);
-      timer = window.setTimeout(() => {
-        parentSetState(v);
-        setState(true);
-      }, 0);
-    };
-    let unmount = () => window.clearTimeout(timer);
-    return [innerclose, unmount];
-  }, [setState, parentSetState, delay]);
-
-  return [state, innerClose, unmount];
-}
-
 export const DatePicker: React.FC<DatePickerProps> = (props) => {
-  const [state, setState] = useState("");
-  const [show, setShow] = useState(false);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setState(e.target.value);
-  };
+  const [date, setDate] = useState('');
+  const [isPanelShow, switchPanelShow] = useState(false);
+  const ref: MutableRefObject<HTMLDivElement> = useRef<HTMLDivElement>();
+  useClickOutside(ref, () => switchPanelShow(false));
 
   const handleClick = () => {
-    setShow(true);
+    switchPanelShow(true);
   };
 
-  const ref = useRef<HTMLDivElement>(null);
-
-  const [st, setst, unmount] = useStateAnimation(setShow, 200);
-  useClickOutside(ref, () => setst(false));
-  const render = useMemo(() => {
-    if (!show) {
-      unmount();
-      return null;
+  const renderPanel = () => {
+    if (isPanelShow) {
+      return <>
+        <div style={{backgroundColor: 'red', transition: 'all 2000 cubic-bezier(0.23, 1, 0.32, 1)'}}>
+          我是弹框
+        </div>
+      </>;
     }
-
-    return <>
-      <div style={{backgroundColor: 'red', transition: 'all 2000 cubic-bezier(0.23, 1, 0.32, 1)'}}>
-        我是弹框
-      </div>
-    </>;
-  }, [show, unmount, st]);
+  };
 
   return (
     <div ref={ref}>
-      <Input>
+      <Input
+        value={date}
+        onClick={handleClick}
+      >
 
       </Input>
-      {/*<input*/}
-        {/*value={state}*/}
-        {/*onChange={handleChange}*/}
-        {/*onClick={handleClick}*/}
-      {/*></input>*/}
-      {render}
+      { renderPanel() }
     </div>
   );
 };
