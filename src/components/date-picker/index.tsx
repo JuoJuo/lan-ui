3dbdfb1bd0f523721ae6351e5af90776b7f5a051
JuoJuo <348107781@qq.com>
2020-09-07 23:23:10 +0800

    record for datepicker

--- src/components/date-picker/index.tsx
+++ src/components/date-picker/index.tsx
@@ -29,6 +29,23 @@ export function useClickOutside(ref: React.RefObject<HTMLElement>, handler: Func
   }, [ref, handler]);
 }
 
+const getDateData = function(year: number, month: number) {
+  const firstDay = new Date(year, month, 1);
+  let weekDay = firstDay.getDay(); //周日，0，周六 6
+  weekDay = weekDay === 0 ? 7 : weekDay;
+  let start = firstDay.getTime() - weekDay * 60 * 60 * 24 * 1000;
+  let arr: number[] = [];
+  for (let i = 0; i < 42; i++) {
+    arr.push(new Date(start + i * 60 * 60 * 24 * 1000).getDate());
+  }
+  let k = -1;
+  return Array.from({ length: 6 }, () => {
+    k++;
+    return arr.slice(k * 7, (k + 1) * 7);
+  });
+};
+getDateData(2020, 8)
+
 export const DatePicker: React.FC<DatePickerProps> = (props) => {
   const [date, setDate] = useState('');
   const [isPanelShow, switchPanelShow] = useState(false);
@@ -41,9 +58,34 @@ export const DatePicker: React.FC<DatePickerProps> = (props) => {
 
   const renderPanel = () => {
     if (isPanelShow) {
+      const dayData = getDateData(new Date().getFullYear(), new Date().getMonth());
+
       return <>
         <div style={{backgroundColor: 'red', transition: 'all 2000 cubic-bezier(0.23, 1, 0.32, 1)'}}>
-          我是弹框
+          <h1>标题</h1>
+
+          <table>
+            <thead>
+              <tr>
+                <th>日</th>
+                <th>一</th>
+                <th>二</th>
+                <th>三</th>
+                <th>四</th>
+                <th>五</th>
+                <th>六</th>
+              </tr>
+            </thead>
+            <tbody>
+              {dayData.map((v, index) => (
+                <tr key={index}>
+                  {v.map((k, i) => (
+                    <td key={i}>{k}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </>;
     }
--- src/stories/date-picker/date-picker.stories.tsx
+++ src/stories/date-picker/date-picker.stories.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import { DatePicker, DatePickerProps } from '../../components/date-picker';
+import { Row } from '../../components/row';
+import { Col } from '../../components/col';
 
 export default {
   title: 'Elements/DatePicker',
   component: DatePicker,
 } as Meta;
 
-const Template: Story<DatePickerProps> = (args) => <DatePicker {...args} />;
+const Template: Story<DatePickerProps> = (args) => <>
+  <Row>
+    <Col span='2'>
+      <DatePicker {...args} />
+    </Col>
+  </Row>
+</>;
 
 export const PickerDemo = Template.bind({});
 PickerDemo.args = {
