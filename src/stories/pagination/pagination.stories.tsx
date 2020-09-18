6743c0e10f0e2c6dacd29905885abe961919e81a
JuoJuo <348107781@qq.com>
2020-09-18 21:24:47 +0800

    record

--- src/stories/pagination/pagination.stories.tsx
+++ src/stories/pagination/pagination.stories.tsx
@@ -3,14 +3,18 @@ import { Story, Meta } from '@storybook/react/types-6-0';
 import { Pagination, PaginationProps } from '../../components/pagination';
 
 export default {
-  title: 'Elements/Pagination',
-  component: Pagination,
+    title: 'Elements/Pagination',
+    component: Pagination,
 } as Meta;
 
 const Template: Story<PaginationProps> = (args) => {
-  return <>
-    <Pagination total={100} pageNo={3} pageSize={10}/>;
-  </>
+    const onChange = (pageNo) => {
+        console.log(pageNo);
+    };
+
+    return <>
+        <Pagination total={100} pageNo={3} pageSize={10} onChange={onChange} />
+    </>
 };
 
 export const Primary = Template.bind({});
